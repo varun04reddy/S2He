9493199eaa3b22972a13367930f30f9c23f6a1be
varun04reddy <115124159+varun04reddy@users.noreply.github.com>
2023-11-19 17:39:12 -0500

    update

---
+++ CSVtoViz.py
@@ -0,0 +1,31 @@
+function visualizeCSV(csvFile) {
+    Papa.parse(csvFile, {
+        complete: function(results) {
+            var data = results.data;
+            var labels = data.map(row => row[0]);
+            var values = data.map(row => row[1]);
+
+            var ctx = document.getElementById('myChart').getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        label: '# of Votes',
+                        data: values,
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                        borderColor: 'rgba(255, 99, 132, 1)',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        }
+    });
+}
